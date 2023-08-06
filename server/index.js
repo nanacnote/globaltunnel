f01118d2fa0b09af9eafae7edca859c97792946f
Owusu <adjeibohyen@hotmail.co.uk>
2023-08-06 20:48:23 +0100

    chore: refactor logic

--- server/index.js
+++ server/index.js
@@ -7,11 +7,6 @@ const InitialiseHTTPServer = require("./server");
 
 const argv = optimist
   .usage("Usage: $0 --port [num]")
-  .options("domain", {
-    default: "localhost",
-    describe:
-      "Specify the base domain name (eg. example.com or gt.example.com)",
-  })
   .options("port", {
     default: "80",
     describe: "listen on this port for outside requests",
@@ -21,8 +16,9 @@ const argv = optimist
     describe: "IP address to bind to",
   })
   .options("domain", {
+    default: "localhost",
     describe:
-      "Specify the base domain name. This is optional if hosting globaltunnel from a regular example.com domain. This is required if hosting a globaltunnel server from a subdomain (i.e. lt.example.dom where clients will be client-app.lt.example.come)",
+      "Specify the base domain name. This is optional if hosting globaltunnel from a regular example.com domain. This is required if hosting a globaltunnel server from a subdomain (i.e. gt.example.com where clients will be client-app.gt.example.come)",
   })
   .options("max-tcp-sockets", {
     default: 10,
@@ -35,24 +31,24 @@ if (argv.help) {
   process.exit();
 }
 
-InitialiseHTTPServer(argv);
-
-process.on("SIGINT", () => {
-  server.close(() => {
-    process.exit();
+InitialiseHTTPServer(argv).then((server) => {
+  process.on("SIGINT", () => {
+    server.close(() => {
+      process.exit();
+    });
   });
-});
 
-process.on("SIGTERM", () => {
-  server.close(() => {
-    process.exit();
+  process.on("SIGTERM", () => {
+    server.close(() => {
+      process.exit();
+    });
   });
-});
 
-process.on("uncaughtException", (err) => {
-  logError(err);
-});
+  process.on("uncaughtException", (err) => {
+    logError(err);
+  });
 
-process.on("unhandledRejection", (reason, promise) => {
-  logError(reason);
+  process.on("unhandledRejection", (reason, promise) => {
+    logError(reason);
+  });
 });
